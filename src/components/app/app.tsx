223213e32c407fb8e260ad7ba0206577bc138511
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-06-18 14:25:59 +0500

    feat(data): getting ingredient data from API

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,16 +2,36 @@ import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import styles from './app.module.css';
-import ingredients from '../../utils/data.json';
+import { useEffect, useState } from 'react';
+
+const URL_API = 'https://norma.nomoreparties.space/api';
 
 function App() {
+  const [ingredients, setIngredients] = useState({ isLoading: true, isError: false, data: null });
+
+  useEffect(() => {
+    const getIngredientData = () => {
+      fetch(`${URL_API}/ingredients`)
+        .then((res) => res.json())
+        .then((res) => res.success && setIngredients({ ...ingredients, isLoading: false, data: res.data }))
+        .catch(() => setIngredients({ ...ingredients, isLoading: false, isError: true }));
+    };
+    getIngredientData();
+  }, []);
+
   return (
     <div className={styles.app}>
-      <AppHeader />
-      <main className={`${styles.main} pr-5 pl-5`}>
-        <BurgerIngredients ingredients={ingredients} />
-        <BurgerConstructor ingredients={ingredients} />
-      </main>
+      {ingredients.isLoading && <>Идет загрузка ингредиентов...</>}
+      {ingredients.isError && <>Ошибка при загрузке ингредиентов</>}
+      {ingredients.data && (
+        <>
+          <AppHeader />
+          <main className={`${styles.main} pr-5 pl-5`}>
+            <BurgerIngredients ingredients={ingredients.data} />
+            <BurgerConstructor ingredients={ingredients.data} />
+          </main>
+        </>
+      )}
     </div>
   );
 }
