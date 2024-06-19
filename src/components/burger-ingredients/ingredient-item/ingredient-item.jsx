75d4c4efd4e14a486e78a0ece3420ba69861d383
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-06-19 08:44:04 +0500

    feat(ingredient-details): added new component ingredient-details; show modal ingredient

--- src/components/burger-ingredients/ingredient-item/ingredient-item.jsx
+++ src/components/burger-ingredients/ingredient-item/ingredient-item.jsx
@@ -1,17 +1,40 @@
 import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './ingredient-item.module.css';
 
-const IngredientItem = ({ name, image, price, counter = null }) => {
+import Modal from '../../modal/modal';
+import { useState } from 'react';
+import IngredientDetails from '../../ingredient-details/ingredient-details';
+
+const IngredientItem = ({ ingredient, counter = null }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const { name, image, price } = ingredient;
+
   return (
-    <li className={styles.ingredient}>
-      {counter && <Counter count={counter} size='default' extraClass={styles.counter} />}
-      <img src={image} alt={name} className='ml-4 mr-4' />
-      <span className={`${styles.price} text text_type_digits-default`}>
-        {price}
-        <CurrencyIcon type='primary' />
-      </span>
-      <span>{name}</span>
-    </li>
+    <>
+      <li className={styles.ingredient} onClick={openModal}>
+        {counter && <Counter count={counter} size='default' extraClass={styles.counter} />}
+        <img src={image} alt={name} className='ml-4 mr-4' />
+        <span className={`${styles.price} text text_type_digits-default`}>
+          {price}
+          <CurrencyIcon type='primary' />
+        </span>
+        <span>{name}</span>
+      </li>
+      {showModal && (
+        <Modal type='ingredient' closeModal={closeModal}>
+          <IngredientDetails ingredient={ingredient} />
+        </Modal>
+      )}
+    </>
   );
 };
 
---
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -0,0 +1,34 @@
+import styles from './ingredient-details.module.css';
+
+const IngredientDetails = ({ ingredient }) => {
+  const { image_large, name, calories, proteins, fat, carbohydrates } = ingredient;
+
+  console.log(ingredient);
+
+  return (
+    <div className={styles.ingredient}>
+      <img src={image_large} alt={name} />
+      <span className='text text_type_main-medium mt-4'>{name}</span>
+      <div className={styles.nutrition}>
+        <div className={styles.nutrition_item}>
+          <span className='text text_type_main-default'>Калории, ккал</span>
+          <span className='text text_type_digits-default'>{calories}</span>
+        </div>
+        <div className={styles.nutrition_item}>
+          <span className='text text_type_main-default'>Белки, г</span>
+          <span className='text text_type_digits-default'>{proteins}</span>
+        </div>
+        <div className={styles.nutrition_item}>
+          <span className='text text_type_main-default'>Жиры, г</span>
+          <span className='text text_type_digits-default'>{fat}</span>
+        </div>
+        <div className={styles.nutrition_item}>
+          <span className='text text_type_main-default'>Углеводы, г</span>
+          <span className='text text_type_digits-default'>{carbohydrates}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IngredientDetails;
