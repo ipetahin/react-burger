e6ee2559e370cb0360b07ee4182739c791de0369
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-06-19 10:18:20 +0500

    feat(order-details): added new component order-details; show modal order

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -2,8 +2,13 @@ import { CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-co
 import styles from './burger-constructor.module.css';
 import ConstructorList from './constructor-list/constructor-list';
 import { ingredientsPropType } from '../../utils/prop-types';
+import useShowModal from '../../hooks/use-show-modal';
+import Modal from '../modal/modal';
+import OrderDetails from '../order-details/order-details';
 
 const BurgerConstructor = ({ ingredients }) => {
+  const { isShowModal, openModal, closeModal } = useShowModal(false);
+
   return (
     <article className={`pt-25 pb-10 pl-4`}>
       <ConstructorList ingredients={ingredients} />
@@ -12,10 +17,15 @@ const BurgerConstructor = ({ ingredients }) => {
           {610}
           <CurrencyIcon type='primary' />
         </span>
-        <Button htmlType='button' type='primary' size='large'>
+        <Button htmlType='button' type='primary' size='large' onClick={openModal}>
           Оформить заказ
         </Button>
       </div>
+      {isShowModal && (
+        <Modal type='order' closeModal={closeModal}>
+          <OrderDetails />
+        </Modal>
+      )}
     </article>
   );
 };
