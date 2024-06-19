1866de7690c8a4baf246bddd500f55de90610d85
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-06-19 09:08:39 +0500

    refactor(css): removed conditional class from modal; added margin to ingredient-details

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -28,8 +28,8 @@ const Modal = ({ type, closeModal, children }) => {
   };
 
   const modalView = (
-    <div className={`${styles.modal} ${type === 'ingredient' && styles.modal_ingredient}`}>
-      {type === 'ingredient' && <h2 className={`${styles.header} text text_type_main-large`}>Детали ингредиента</h2>}
+    <div className={styles.modal}>
+      {type === 'ingredient' && <h2 className={`${styles.header} mt-10 ml-10 mr-10 text text_type_main-large`}>Детали ингредиента</h2>}
       <span className={styles.close} onClick={handleClickCloseModal}>
         <CloseIcon />
       </span>
