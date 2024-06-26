6b1f4bb0389f2b817cb6ecb73b1fc0ccd43004dc
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-06-26 16:23:41 +0500

    refactor(ingredient-item): added handleShowIngredientDetails

--- src/components/burger-ingredients/ingredient-item/ingredient-item.jsx
+++ src/components/burger-ingredients/ingredient-item/ingredient-item.jsx
@@ -5,15 +5,24 @@ import Modal from '../../modal/modal';
 import IngredientDetails from '../../ingredient-details/ingredient-details';
 import useShowModal from '../../../hooks/use-show-modal';
 import { ingredientItemPropType } from '../../../utils/prop-types';
+import { useDispatch } from 'react-redux';
+import { set } from '../../../services/slices/ingredient-details-slice';
 
 const IngredientItem = ({ ingredient, counter = null }) => {
   const { isShowModal, openModal, closeModal } = useShowModal(false);
 
   const { name, image, price } = ingredient;
 
+  const dispatch = useDispatch();
+
+  const handleShowIngredientDetails = () => {
+    dispatch(set(ingredient));
+    openModal();
+  };
+
   return (
     <>
-      <li className={styles.ingredient} onClick={openModal}>
+      <li className={styles.ingredient} onClick={handleShowIngredientDetails}>
         {counter && <Counter count={counter} size='default' extraClass={styles.counter} />}
         <img src={image} alt={name} className='ml-4 mr-4' />
         <span className={`${styles.price} text text_type_digits-default`}>
@@ -24,7 +33,7 @@ const IngredientItem = ({ ingredient, counter = null }) => {
       </li>
       {isShowModal && (
         <Modal text='Детали ингредиента' closeModal={closeModal}>
-          <IngredientDetails ingredient={ingredient} />
+          <IngredientDetails />
         </Modal>
       )}
     </>
