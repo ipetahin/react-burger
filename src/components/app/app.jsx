4c0d1b91e0609f35f7759cf15f40958d77df2f6b
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-06-26 08:22:20 +0500

    refactor(burger-ingredients): moved the ingredients request to burger-ingredients

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,41 +1,17 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import { request, success, failure } from '../../services/slices/ingredients-slice';
-
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import styles from './app.module.css';
-import fetchApi from '../../utils/fetch-api';
 
 function App() {
-  const { isLoading, isError, data } = useSelector((state) => state.ingredients);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(request());
-
-    fetchApi('ingredients')
-      .then((data) => dispatch(success(data)))
-      .catch(() => dispatch(failure()));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <div className={styles.app}>
       <>
         <AppHeader />
-        {/* TODO: spinner */}
-        {isLoading && <>Идет загрузка ингредиентов...</>}
-        {/* TODO: show error */}
-        {isError && <>Ошибка при загрузке ингредиентов</>}
-        {data && (
-          <main className={`${styles.main} pr-5 pl-5`}>
-            <BurgerIngredients ingredients={data} />
-            <BurgerConstructor ingredients={data} />
-          </main>
-        )}
+        <main className={`${styles.main} pr-5 pl-5`}>
+          <BurgerIngredients />
+          {/* <BurgerConstructor /> */}
+        </main>
       </>
     </div>
   );
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,24 +1,54 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { request, success, failure } from '../../services/slices/ingredients-slice';
+
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './burger-ingredients.module.css';
 
 import IngredientGroup from './ingredient-group/ingredient-group';
 import { ingredientsPropType } from '../../utils/prop-types';
+import fetchApi from '../../utils/fetch-api';
 
-const BurgerIngredients = ({ ingredients }) => {
+const BurgerIngredients = () => {
+  const { isLoading, isError, data } = useSelector((state) => state.ingredients);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(request());
+
+    fetchApi('ingredients')
+      .then((data) => dispatch(success(data)))
+      .catch(() => dispatch(failure()));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log(data)
+
   return (
-    <article className={`pt-10 pb-10`}>
-      <h1 className='text text_type_main-large mb-5'>Соберите бургер</h1>
-      <div className={`${styles.tabs} mb-10`}>
-        <Tab key='bunTab' active>Булки</Tab>
-        <Tab key='sauceTab'>Соусы</Tab>
-        <Tab key='mainTab'>Начинки</Tab>
-      </div>
-      <div className={`${styles.groups}`}>
-        <IngredientGroup key='bunGroup' ingredients={ingredients} name='Булки' type='bun' />
-        <IngredientGroup key='sauceGroup' ingredients={ingredients} name='Соусы' type='sauce' />
-        <IngredientGroup key='mainGroup' ingredients={ingredients} name='Начинки' type='main' />
-      </div>
-    </article>
+    <>
+      {/* TODO: spinner */}
+      {isLoading && <>Идет загрузка ингредиентов...</>}
+      {/* TODO: show error */}
+      {isError && <>Ошибка при загрузке ингредиентов</>}
+      {data && (
+        <article className={`pt-10 pb-10`}>
+          <h1 className='text text_type_main-large mb-5'>Соберите бургер</h1>
+          <div className={`${styles.tabs} mb-10`}>
+            <Tab key='bunTab' active>
+              Булки
+            </Tab>
+            <Tab key='sauceTab'>Соусы</Tab>
+            <Tab key='mainTab'>Начинки</Tab>
+          </div>
+          <div className={`${styles.groups}`}>
+            <IngredientGroup key='bunGroup' ingredients={data} name='Булки' type='bun' />
+            <IngredientGroup key='sauceGroup' ingredients={data} name='Соусы' type='sauce' />
+            <IngredientGroup key='mainGroup' ingredients={data} name='Начинки' type='main' />
+          </div>
+        </article>
+      )}
+    </>
   );
 };
 
