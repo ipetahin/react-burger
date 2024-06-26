398797e95b2ef7627330bbeeb42daaa85a65935f
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-06-26 16:30:23 +0500

    perf(strict-mode): strict mode disabled to reduce rerendering

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,7 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </Provider>
 );
 
