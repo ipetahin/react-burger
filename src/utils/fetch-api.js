ff60ad1a70ce42cfea66527b2324b05ff5e7b412
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-06-26 15:35:15 +0500

    refactor(fetch-api): added a request to send the post method

--- src/utils/fetch-api.js
+++ src/utils/fetch-api.js
@@ -1,8 +1,20 @@
-const fetchApi = (endpoint) => {
+const fetchApi = (endpoint, postData = null) => {
   const url = `https://norma.nomoreparties.space/api/${endpoint}`;
-  return fetch(url)
+  let options = null;
+
+  if (postData) {
+    options = {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  }
+
+  return fetch(url, { ...options })
     .then((res) => res.json())
-    .then((json) => json.success && json.data);
+    .then((data) => data.success && data);
 };
 
 export default fetchApi;
