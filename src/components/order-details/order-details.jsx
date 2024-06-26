3a47b4890be0c1f70fc4a66f3c7ffd56a970530c
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-06-26 15:59:53 +0500

    feat(order-details): added getting order number from store

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,10 +1,13 @@
 import styles from './order-details.module.css';
 import done from '../../images/done.gif';
+import { useSelector } from 'react-redux';
 
 const OrderDetails = () => {
+  const { number } = useSelector((store) => store.orderDetails.data.order);
+
   return (
     <div className={styles.order}>
-      <span className={`${styles.order_number} text text_type_digits-large`}>034536</span>
+      <span className={`${styles.order_number} text text_type_digits-large`}>{number}</span>
       <span className='text text_type_main-medium mt-8'>идентификатор заказа</span>
       <img src={done} alt='done' className='mt-15' />
       <span className='text text_type_main-default mt-15'>Ваш заказ начали готовить</span>
