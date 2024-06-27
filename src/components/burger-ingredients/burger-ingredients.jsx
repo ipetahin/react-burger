632874e005eec9d9068f0a416c692095d55f8e88
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-06-27 08:55:37 +0500

    feat(scroll-ingredients): added smooth tab change when scrolling; added transition when clicking on a tab

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { request, success, failure } from '../../services/slices/burger-ingredients-slice';
@@ -12,6 +12,11 @@ import fetchApi from '../../utils/fetch-api';
 const BurgerIngredients = () => {
   const { isLoading, isError, data } = useSelector((state) => state.burgerIngredients);
   const dispatch = useDispatch();
+  const tabsRef = useRef();
+  const groupBunRef = useRef();
+  const groupSauceRef = useRef();
+  const groupMainRef = useRef();
+  const [activeTab, setActiveTab] = useState('bun');
 
   useEffect(() => {
     dispatch(request());
@@ -22,6 +27,47 @@ const BurgerIngredients = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const handleScrollIngredientGroup = () => {
+    const tabsTopCoord = tabsRef.current.getBoundingClientRect().top;
+    const bunTopCoord = groupBunRef.current.getBoundingClientRect().top;
+    const sauceTopCoord = groupSauceRef.current.getBoundingClientRect().top;
+    const mainTopCoord = groupMainRef.current.getBoundingClientRect().top;
+    const arr = [bunTopCoord, sauceTopCoord, mainTopCoord];
+    const closestIndex = arr.findIndex((elem) => elem === arr.reduce((prev, curr) => (Math.abs(curr - tabsTopCoord) < Math.abs(prev - tabsTopCoord) ? curr : prev)));
+    switch (closestIndex) {
+      case 0:
+        if (activeTab !== 'bun') setActiveTab('bun');
+        break;
+      case 1:
+        if (activeTab !== 'sauce') setActiveTab('sauce');
+        break;
+      case 2:
+        if (activeTab !== 'main') setActiveTab('main');
+        break;
+      default:
+        setActiveTab('bun');
+        break;
+    }
+  };
+
+  const handleClickTab = (tab) => {
+    if (activeTab !== 'bun') setActiveTab('bun');
+    switch (tab) {
+      case 'bun':
+        groupBunRef.current.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'sauce':
+        groupSauceRef.current.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'main':
+        groupMainRef.current.scrollIntoView({ behavior: 'smooth' });
+        break;
+      default:
+        groupBunRef.current.scrollIntoView({ behavior: 'smooth' });
+        break;
+    }
+  };
+
   return (
     <>
       {/* TODO: spinner */}
@@ -31,17 +77,21 @@ const BurgerIngredients = () => {
       {data && (
         <article className={`pt-10 pb-10`}>
           <h1 className='text text_type_main-large mb-5'>Соберите бургер</h1>
-          <div className={`${styles.tabs} mb-10`}>
-            <Tab key='bunTab' active>
+          <div ref={tabsRef} className={`${styles.tabs} mb-10`}>
+            <Tab value='bun' active={activeTab === 'bun'} onClick={handleClickTab}>
               Булки
             </Tab>
-            <Tab key='sauceTab'>Соусы</Tab>
-            <Tab key='mainTab'>Начинки</Tab>
+            <Tab value='sauce' active={activeTab === 'sauce'} onClick={handleClickTab}>
+              Соусы
+            </Tab>
+            <Tab value='main' active={activeTab === 'main'} onClick={handleClickTab}>
+              Начинки
+            </Tab>
           </div>
-          <div className={`${styles.groups}`}>
-            <IngredientGroup key='bunGroup' ingredients={data} name='Булки' type='bun' />
-            <IngredientGroup key='sauceGroup' ingredients={data} name='Соусы' type='sauce' />
-            <IngredientGroup key='mainGroup' ingredients={data} name='Начинки' type='main' />
+          <div className={`${styles.group}`} onScroll={handleScrollIngredientGroup}>
+            <IngredientGroup ingredients={data} title='Булки' type='bun' ref={groupBunRef} />
+            <IngredientGroup ingredients={data} title='Соусы' type='sauce' ref={groupSauceRef} />
+            <IngredientGroup ingredients={data} title='Начинки' type='main' ref={groupMainRef} />
           </div>
         </article>
       )}
--- src/components/burger-ingredients/ingredient-group/ingredient-group.jsx
+++ src/components/burger-ingredients/ingredient-group/ingredient-group.jsx
@@ -1,11 +1,12 @@
+import { forwardRef } from 'react';
 import styles from './ingredient-group.module.css';
 import IngredientItem from '../ingredient-item/ingredient-item';
 import { ingredientGroupPropType } from '../../../utils/prop-types';
 
-const IngredientGroup = ({ ingredients, name, type }) => {
+const IngredientGroup = forwardRef(({ ingredients, title, type }, ref) => {
   return (
     <section>
-      <h2 className='text text_type_main-medium'>{name}</h2>
+      <h2 className='text text_type_main-medium' ref={ref}>{title}</h2>
       <ul className={`${styles.group} mt-6 mr-2 mb-10 ml-4`}>
         {ingredients
           .filter((ingredient) => ingredient.type === type)
@@ -15,7 +16,7 @@ const IngredientGroup = ({ ingredients, name, type }) => {
       </ul>
     </section>
   );
-};
+});
 
 IngredientGroup.propTypes = {
   ...ingredientGroupPropType,
