596f8a3edde70a79b89098b7b8b9139ce58e6f0e
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-06-28 10:01:46 +0500

    feat(order-details): added status check

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -3,15 +3,23 @@ import done from '../../images/done.gif';
 import { useSelector } from 'react-redux';
 
 const OrderDetails = () => {
-  const { number } = useSelector((store) => store.orderDetails.data.order);
+  const {isLoading, isError, data} = useSelector((store) => store.orderDetails);
 
   return (
     <div className={styles.order}>
-      <span className={`${styles.order_number} text text_type_digits-large`}>{number}</span>
-      <span className='text text_type_main-medium mt-8'>идентификатор заказа</span>
-      <img src={done} alt='done' className='mt-15' />
-      <span className='text text_type_main-default mt-15'>Ваш заказ начали готовить</span>
-      <span className='text text_type_main-default text_color_inactive mt-2'>Дождитесь готовности на орбитальной станции</span>
+      {/* TODO: spinner */}
+      {isLoading && <>Отправляем заказ...</>}
+      {/* TODO: show error */}
+      {isError && <>Ошибка при отпраке заказа</>}
+      {data && (
+        <>
+          <span className={`${styles.order_number} text text_type_digits-large`}>{data.order.number}</span>
+          <span className='text text_type_main-medium mt-8'>идентификатор заказа</span>
+          <img src={done} alt='done' className='mt-15' />
+          <span className='text text_type_main-default mt-15'>Ваш заказ начали готовить</span>
+          <span className='text text_type_main-default text_color_inactive mt-2'>Дождитесь готовности на орбитальной станции</span>
+        </>
+      )}
     </div>
   );
 };
