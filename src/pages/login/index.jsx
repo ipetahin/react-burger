25a6fa0183dd501e11ba6b9be8743b030cd777ab
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-07-12 11:14:45 +0500

    feat(login): added login authorization

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,16 +1,26 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { EmailInput, PasswordInput, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 
 import useFormData from '../../hooks/use-form-data';
 import styles from './login.module.css';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../services/slices/user-slice';
 
 export default function LoginPage() {
-  const [formData, onChangeFormData] = useFormData({ username: '', email: '', password: '' });
+  const [formData, onChangeFormData] = useFormData({ email: '', password: '' });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(loginUser(formData));
+    navigate('/');
+  };
 
   return (
     <main className={`${styles.main}`}>
       <h1 className='text text_type_main-medium'>Вход</h1>
-      <form className={`${styles.form} mt-6 mb-20`}>
+      <form className={`${styles.form} mt-6 mb-20`} onSubmit={handleSubmit}>
         <EmailInput onChange={onChangeFormData} value={formData.email} name='email' isIcon={false} />
         <PasswordInput onChange={onChangeFormData} value={formData.password} name='password' />
         <Button htmlType='submit' type='primary' size='medium'>
