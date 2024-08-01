dc24852892be17e5ba92c48a0b14c1ee1642d652
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-08-01 11:10:56 +0500

    refactor(burger-constructor): typified preparedData and totalPrice

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -7,18 +7,17 @@ import styles from './burger-constructor.module.css';
 import ConstructorList from './constructor-list/constructor-list';
 import { sendOrder } from '../../services/slices/order-details-slice';
 import { addIngredient } from '../../services/slices/burger-сonstructor-slice';
-import { Ingredient } from '../../types';
+import { ArrayData, Ingredient } from '../../types';
 
 const BurgerConstructor = () => {
   const { bun, ingredients } = useSelector((store: any) => store.burgerConstructor);
-  const { isLoading } = useSelector((store: any) => store.burgerIngredients);
   const { user } = useSelector((store: any) => store.user);
   const navigate = useNavigate();
   const location = useLocation();
 
   const dispatch: any = useDispatch();
 
-  const totalPrice = useMemo(() => {
+  const totalPrice: number = useMemo(() => {
     const bunPrice = bun ? bun.price * 2 : 0;
     return bunPrice + ingredients.reduce((acc: number, ingredient: Ingredient) => acc + ingredient.price, 0);
   }, [bun, ingredients]);
@@ -29,7 +28,7 @@ const BurgerConstructor = () => {
     }
 
     if (bun && ingredients.length) {
-      const preparedData = { ingredients: [bun._id, ...ingredients.map((ingredient: Ingredient) => ingredient._id), bun._id] };
+      const preparedData: ArrayData = { ingredients: [bun._id, ...ingredients.map((ingredient: Ingredient) => ingredient._id), bun._id] };
       dispatch(sendOrder(preparedData));
       navigate('/', { state: { backgroundLocation: location } });
     }
@@ -40,20 +39,18 @@ const BurgerConstructor = () => {
   };
 
   return (
-    !isLoading && (
-      <article className={`pt-25 pb-10 pl-4`}>
-        <ConstructorList onDropHandler={handleIngredientDrop} />
-        <div className={styles.order}>
-          <span className={`${styles.total} text text_type_digits-medium`}>
-            {totalPrice}
-            <CurrencyIcon type='primary' />
-          </span>
-          <Button htmlType='button' type='primary' size='large' onClick={handleSubmitOrder}>
-            Оформить заказ
-          </Button>
-        </div>
-      </article>
-    )
+    <article className={`pt-25 pb-10 pl-4`}>
+      <ConstructorList onDropHandler={handleIngredientDrop} />
+      <div className={styles.order}>
+        <span className={`${styles.total} text text_type_digits-medium`}>
+          {totalPrice}
+          <CurrencyIcon type='primary' />
+        </span>
+        <Button htmlType='button' type='primary' size='large' onClick={handleSubmitOrder}>
+          Оформить заказ
+        </Button>
+      </div>
+    </article>
   );
 };
 
