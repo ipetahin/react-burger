240906f9889e6979a135c1ac4dc0680f07032967
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-08-01 15:05:44 +0500

    refactor(types): added types Ingredients, ConstructorIngredients, IngredientType; added interfaces ConstructorIngredient, BurgerConstructorStore, User, Owner, UserStore, BurgerIngredientStore, IngredientDetailsStore, Order, OrderDetailsStore

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,7 @@
 export interface Ingredient {
-  _id?: string;
+  _id: string;
   name: string;
-  type: string;
+  type: IngredientType;
   proteins: number;
   fat: number;
   carbohydrates: number;
@@ -11,10 +11,66 @@ export interface Ingredient {
   image_mobile: string;
   image_large: string;
   __v: number;
+}
+
+export type Ingredients = Array<Ingredient>;
+
+export interface ConstructorIngredient extends Ingredient {
   id: string;
 }
 
+export type ConstructorIngredients = Array<ConstructorIngredient>;
+
+export interface BurgerConstructorStore {
+  bun: ConstructorIngredient | null;
+  ingredients: ConstructorIngredients;
+}
+
+export interface User {
+  email: string;
+  name: string;
+  [key: string]: string;
+}
+
+interface Owner extends User {
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface UserStore {
+  user: User | null;
+  isAuthChecked: boolean;
+}
+
+export interface BurgerIngredientStore {
+  data: Ingredients | null;
+  isLoading: boolean;
+  isError: boolean;
+}
+
+export type IngredientDetailsStore = Ingredient | null;
+
+interface Order {
+  ingredients: Ingredients;
+  _id: string;
+  owner: Owner;
+  status: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+  number: number;
+  price: number;
+}
+
+export interface OrderDetailsStore {
+  data: { success: boolean; name: string; order: Order } | null;
+  isLoading: boolean;
+  isError: boolean;
+}
+
 export type ConstructorItemType = 'top' | 'bottom';
+
+export type IngredientType = 'bun' | 'sauce' | 'main';
 
 export interface FormData {
   [name: string]: string;
