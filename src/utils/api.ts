3159289c92504c0b5700228ac3f4789c2430755a
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-08-02 13:36:22 +0500

    refactor(api): added generics for requests

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,54 +1,66 @@
-import { ArrayData, FormData, HTTPMethods, Options, ResponseError } from '../types';
+import { FormData, ArrayData } from '../types';
+import {
+  RequestData,
+  HTTPMethods,
+  Options,
+  ServerIngredientsResponse,
+  ServerUserResponse,
+  ServerRefreshResponse,
+  ServerMessageResponse,
+  ServerResponse,
+  ServerOrderResponse,
+} from '../types/api';
 
-export default function request(endpoint: string, options?: Options) {
+export default function request<T>(endpoint: string, options?: Options): Promise<T> {
   const baseUrl = 'https://norma.nomoreparties.space/api/';
-  const checkResponse = (res: Response) => {
+  const checkResponse = <T>(res: Response): Promise<T> => {
     return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
   };
 
-  return fetch(`${baseUrl}${endpoint}`, options).then(checkResponse);
+  return fetch(`${baseUrl}${endpoint}`, options).then(checkResponse<T>);
 }
 
-const requestPost = (endpoint: string, data: FormData) => {
-  return request(endpoint, {
+const requestPost = <T>(endpoint: string, data: FormData): Promise<T> => {
+  return request<T>(endpoint, {
     method: 'POST',
     body: JSON.stringify(data),
     headers: {
       'Content-Type': 'application/json;charset=utf-8',
     },
-  }).then((data) => (data.success ? data : Promise.reject(data)));
+  }).then((data) => ((data as ServerResponse).success ? data : Promise.reject(data)));
 };
 
-const requestWithRefresh = async (endpoint: string, options: Options) => {
+const requestWithRefresh = async <T>(endpoint: string, options: Options): Promise<T> => {
   const refreshToken = () => {
     const token = localStorage.getItem('refreshToken');
     if (token) {
-      return requestPost('auth/token', { token }).then((refreshData) => {
+      return requestPost<ServerRefreshResponse>('auth/token', { token }).then((refreshData) => {
         localStorage.setItem('refreshToken', refreshData.refreshToken);
         localStorage.setItem('accessToken', refreshData.accessToken);
         return refreshData;
       });
+    } else {
+      return Promise.reject();
     }
   };
 
   try {
-    return await request(endpoint, options);
-  } catch (err: unknown) {
-    const responseError = err as ResponseError;
-    if (responseError.message === 'jwt expired') {
+    return await request<T>(endpoint, options);
+  } catch (err) {
+    if ((err as { message: string }).message === 'jwt expired') {
       const refreshData = await refreshToken();
       options.headers.authorization = refreshData.accessToken;
-      return await request(endpoint, options);
+      return await request<T>(endpoint, options);
     } else {
       return Promise.reject(err);
     }
   }
 };
 
-const requestWithAccessToken = (endpoint: string, method: HTTPMethods = 'GET', data?: FormData | ArrayData) => {
+const requestWithAccessToken = <T>(endpoint: string, method: HTTPMethods = 'GET', data?: RequestData): Promise<T> => {
   const accessToken = localStorage.getItem('accessToken');
   if (accessToken) {
-    return requestWithRefresh(endpoint, {
+    return requestWithRefresh<T>(endpoint, {
       method,
       body: JSON.stringify(data),
       headers: {
@@ -56,26 +68,29 @@ const requestWithAccessToken = (endpoint: string, method: HTTPMethods = 'GET', d
         authorization: accessToken,
       },
     });
+  } else {
+    return Promise.reject();
   }
 };
 
 export const fetchIngredients = () => {
-  return request('ingredients').then((data) => data.success && data.data);
+  return request<ServerIngredientsResponse>('ingredients').then((data) => data.success && data.data);
 };
 
-export const login = (data: FormData) => requestPost('auth/login', data);
-export const register = (data: FormData) => requestPost('auth/register', data);
+export const login = (data: FormData) => requestPost<ServerUserResponse>('auth/login', data);
+export const register = (data: FormData) => requestPost<ServerUserResponse>('auth/register', data);
+export const requestUser = () => requestWithAccessToken<ServerUserResponse>('auth/user');
+export const requestUpdateUser = (data: FormData) => requestWithAccessToken<ServerUserResponse>('auth/user', 'PATCH', data);
 
+export const passwordReset = (data: FormData) => requestPost<ServerMessageResponse>('password-reset/reset', data);
+export const passwordResetRequest = (data: FormData) => requestPost<ServerMessageResponse>('password-reset', data);
 export const logout = () => {
   const token = localStorage.getItem('refreshToken');
   if (token) {
-    return requestPost('auth/logout', { token });
+    return requestPost<ServerMessageResponse>('auth/logout', { token });
+  } else {
+    Promise.reject();
   }
 };
 
-export const passwordResetRequest = (data: FormData) => requestPost('password-reset', data);
-export const passwordReset = (data: FormData) => requestPost('password-reset/reset', data);
-
-export const requestUser = () => requestWithAccessToken('auth/user');
-export const requestSendOrder = (data: ArrayData) => requestWithAccessToken('orders', 'POST', data);
-export const requestUpdateUser = (data: FormData) => requestWithAccessToken('auth/user', 'PATCH', data);
+export const requestSendOrder = (data: ArrayData) => requestWithAccessToken<ServerOrderResponse>('orders', 'POST', data);
