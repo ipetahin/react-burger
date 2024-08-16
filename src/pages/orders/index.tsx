ca7e385e03cc848ec7c383d6acde57f4b0b514a5
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-08-16 11:05:45 +0500

    feat(orders): added orders page

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -1,3 +1,21 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import OrderList from '../../components/order-list';
+import { getIngredients } from '../../services/slices/burger-ingredients-slice';
+import styles from './orders.module.css';
+
 export default function OrdersPage() {
-  return <h1>Тут будет история заказов</h1>;
+  const dispatch: any = useDispatch();
+
+  useEffect(() => {
+    dispatch(getIngredients());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  
+  return (
+    <div className={styles.orders}>
+      <OrderList isShowStatus={true} />
+    </div>
+  );
 }
