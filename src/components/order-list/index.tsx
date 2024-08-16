6f2c8c5829b45d306e6e5cef52d533d4552812a2
Igor.Petahin <Igor.Petahin@yandex.ru>
2024-08-16 16:06:22 +0500

    refactor(order-list): added showing less than 6 ingredient previews; added link for cards to order-info

--- src/components/order-list/index.tsx
+++ src/components/order-list/index.tsx
@@ -1,16 +1,18 @@
-import styles from './order-list.module.css';
-import { CurrencyIcon, FormattedDate } from '@ya.praktikum/react-developer-burger-ui-components';
+import { FC } from 'react';
 import { useSelector } from 'react-redux';
+import { Link, useLocation } from 'react-router-dom';
+import { GridLoader } from 'react-spinners';
+import { CurrencyIcon, FormattedDate } from '@ya.praktikum/react-developer-burger-ui-components';
+
+import styles from './order-list.module.css';
 import { Store } from '../../types';
 import { ordersResponse } from '../../types/orders';
-import { GridLoader } from 'react-spinners';
-import { FC } from 'react';
 
-const statuses = {
-  done: 'Выполнен',
-  created: 'Создан',
-  'in-work': 'Готовится',
-};
+export enum Statuses {
+  done = 'Выполнен',
+  created = 'Создан',
+  'in-work' = 'Готовится',
+}
 
 interface OrderListProps {
   isShowStatus: boolean;
@@ -18,6 +20,7 @@ interface OrderListProps {
 
 const OrderList: FC<OrderListProps> = ({ isShowStatus }) => {
   const { isLoading, isError, data } = useSelector((store: Store) => store.burgerIngredients);
+  const location = useLocation();
 
   return (
     <ul className={styles.list}>
@@ -27,31 +30,38 @@ const OrderList: FC<OrderListProps> = ({ isShowStatus }) => {
         ordersResponse.orders.map((order) => {
           const ingredients = order.ingredients.map((ingredientId) => data?.find((ingredient) => ingredient._id === ingredientId));
           return (
-            <li className={styles.card}>
-              <div className={styles.id}>
-                <span className='text text_type_digits-default'>{`#0${order.number}`}</span>
-                <FormattedDate className='text text_type_main-default text_color_inactive' date={new Date(order.createdAt)} />
-              </div>
-              <div className={styles.info}>
-                <span className='text text_type_main-medium'>Death Star Starship Main бургер</span>
-                {isShowStatus && (order.status === 'done' ? <span className={styles.done}>{statuses['done']}</span> : <span>{statuses[order.status]}</span>)}
-              </div>
-              <div className={styles.components}>
-                <ul className={styles.ingredients}>
-                  {ingredients.map((ingredient, i) => {
-                    return i < 6 ? (
-                      <li className={styles.ingredient} style={{ transform: `translate(${-16 * i}px`, zIndex: `${6 - i}` }}>
-                        <img src={ingredient?.image_mobile} alt={ingredient?.name} className={styles.preview} />
-                      </li>
-                    ) : null;
-                  })}
-                </ul>
-                <div className={styles.price}>
-                  <span className='text text_type_digits-default'>{ingredients.reduce((acc, ingredient) => acc + (ingredient?.price || 0), 0)}</span>
-                  <CurrencyIcon type='primary' />
+            <Link className={styles.link} key={order._id} to={`/feed/${order._id}`} state={{ backgroundLocation: location }}>
+              <li className={styles.card}>
+                <div className={styles.id}>
+                  <span className='text text_type_digits-default'>{`#0${order.number}`}</span>
+                  <FormattedDate className='text text_type_main-default text_color_inactive' date={new Date(order.createdAt)} />
                 </div>
-              </div>
-            </li>
+                <div className={styles.info}>
+                  <span className='text text_type_main-medium'>Death Star Starship Main бургер</span>
+                  {isShowStatus && (order.status === 'done' ? <span className={styles.done}>{Statuses['done']}</span> : <span>{Statuses[order.status]}</span>)}
+                </div>
+                <div className={styles.components}>
+                  <ul className={styles.ingredients}>
+                    {ingredients.map((ingredient, i) => {
+                      return i < 5 ? (
+                        <li className={styles.ingredient} style={{ transform: `translate(${-16 * i}px`, zIndex: `${6 - i}` }}>
+                          <img src={ingredient?.image_mobile} alt={ingredient?.name} className={styles.preview} />
+                        </li>
+                      ) : i === 5 ? (
+                        <li className={styles.ingredient} style={{ transform: `translate(${-16 * i}px`, zIndex: 1 }}>
+                          <img src={ingredient?.image_mobile} alt={ingredient?.name} className={`${styles.preview} ${styles['preview-overlay']}`} />
+                          <span className={`${styles['preview-amount']} text`}>{`+${ingredients.length - 5}`}</span>
+                        </li>
+                      ) : null;
+                    })}
+                  </ul>
+                  <div className={styles.price}>
+                    <span className='text text_type_digits-default'>{ingredients.reduce((acc, ingredient) => acc + (ingredient?.price || 0), 0)}</span>
+                    <CurrencyIcon type='primary' />
+                  </div>
+                </div>
+              </li>
+            </Link>
           );
         })}
     </ul>
